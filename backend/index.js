e2dc58f26a431fb2cf870f1cb47284b8e7cf0b8d
Sankalpa_01 <sankalpaanda@gmail.com>
2025-05-17 22:22:48 +0530

    frontend deployed.

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://portfolio-website-rose-six-68.vercel.app/",
   credentials: true,
 }));
 
